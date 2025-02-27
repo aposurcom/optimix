5e8fe4b5033bffff43e333b8305ee4903092c7c3
aposurcom <aposurcom@gmail.com>
2025-02-27 17:33:03 +0200

    Add files via upload

---
+++ func.py
@@ -0,0 +1,49 @@
+from ortools.linear_solver import pywraplp
+
+
+def product_mix_optimization(product_A_material=4, product_B_material=2, material_max=60, product_A_labor=2, product_B_labor=3, labor_max=60):
+    # Create solver
+    solver = pywraplp.Solver.CreateSolver('GLOP')  # GLOP is for linear programming
+
+    if not solver:
+        return
+
+    # Decision variables: Number of units to produce (continuous variables)
+    # product_A and product_B are the number of units to produce.
+    # They must be ≥ 0 (you can’t produce a negative amount).
+    product_A = solver.NumVar(0, solver.infinity(), 'Product_A')
+    product_B = solver.NumVar(0, solver.infinity(), 'Product_B')
+
+    print("Number of variables =", solver.NumVariables())
+
+    # Constraints (Available Resources)
+    # Material Constraint: 4A + 6B ≤ 60
+    solver.Add(product_A_material * product_A + product_B_material * product_B <= material_max) # Material Limit ProductA needs 4qty and ProductB needs 6qty max 60qty
+
+    # Labor Constraint: 2A + 3B ≤ 30
+    solver.Add(product_A_labor * product_A + product_B_labor * product_B <= labor_max) # Labor Limit ProductA needs 2h and productB needs 3 max 60h
+
+    print("Number of constraints =", solver.NumConstraints())
+
+    # Objective Function: Maximize Profit (30A + 40B)
+    solver.Maximize(30 * product_A + 40 * product_B) # ProductA $30 per unit and ProductB $40 per unit
+
+    # Solve the problem
+    status = solver.Solve()
+    result = ""
+
+    # Save and print Results
+    if status == pywraplp.Solver.OPTIMAL:
+        result = f"Optimal production of Product A: {product_A.solution_value():.2f}\n"
+        result += f"Optimal production of Product B: {product_B.solution_value():.2f}\n"
+        result += f"Maximum Profit: {solver.Objective().Value():.2f}\n"
+    else:
+        result = "No optimal solution found."
+
+    print(result)
+    
+    return result
+
+if __name__ == '__main__':
+    # Run the function
+    product_mix_optimization()
---
+++ gui.py
@@ -0,0 +1,90 @@
+import tkinter as tk
+from tkinter import ttk
+import func
+
+class MainApp(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Main Application")
+        # self.geometry("400x300")
+
+        # Tkinter labels and input fields in 2 columns of product_A_material=4, product_B_material=2, material_max=60, product_A_labor=2, product_B_labor=3, labor_max=60
+        self.lbl_product_A_material = ttk.Label(self, text="Product A Material(qty):")
+        self.lbl_product_A_material.grid(row=0, column=0, pady=5)
+        self.product_A_material = ttk.Entry(self)
+        self.product_A_material.grid(row=0, column=1, pady=5)
+
+        self.lbl_product_B_material = ttk.Label(self, text="Product B Material(qty):")
+        self.lbl_product_B_material.grid(row=1, column=0, pady=5)
+        self.product_B_material = ttk.Entry(self)
+        self.product_B_material.grid(row=1, column=1, pady=5)
+
+        self.lbl_material_max = ttk.Label(self, text="Material Max(qty):")
+        self.lbl_material_max.grid(row=2, column=0, pady=5)
+        self.material_max = ttk.Entry(self)
+        self.material_max.grid(row=2, column=1, pady=5)
+
+        self.lbl_product_A_labor = ttk.Label(self, text="Product A Labor(h):")
+        self.lbl_product_A_labor.grid(row=3, column=0, pady=5)
+        self.product_A_labor = ttk.Entry(self)
+        self.product_A_labor.grid(row=3, column=1, pady=5)
+
+        self.lbl_product_B_labor = ttk.Label(self, text="Product B Labor(h):")
+        self.lbl_product_B_labor.grid(row=4, column=0, pady=5)
+        self.product_B_labor = ttk.Entry(self)
+        self.product_B_labor.grid(row=4, column=1, pady=5)
+
+        self.lbl_labor_max = ttk.Label(self, text="Labor Max(h):")
+        self.lbl_labor_max.grid(row=5, column=0, pady=5)
+        self.labor_max = ttk.Entry(self)
+        self.labor_max.grid(row=5, column=1, pady=5)
+
+        self.button = ttk.Button(self, text="Find answer", command=self.on_button_click)
+        self.button.grid(row=6, column=0, columnspan=2, pady=5)
+
+        self.lbl_result = ttk.Label(self, text="Instructions:\n1. Input required variables\n2. Press \"Find anwer\"!")
+        self.lbl_result.grid(row=7, column=0, columnspan=2, pady=5)
+
+    def on_button_click(self):
+        # Check if all inputs are empty
+        if self.product_A_material.get() == "" or self.product_B_material.get() == "" or self.material_max.get() == "" or self.product_A_labor.get() == "" or self.product_B_labor.get() == "" or self.labor_max.get() == "":
+            # set default values
+            self.product_A_material.insert(0, 4)
+            self.product_B_material.insert(0, 2)
+            self.material_max.insert(0, 60)
+            self.product_A_labor.insert(0, 2)
+            self.product_B_labor.insert(0, 3)
+            self.labor_max.insert(0, 60)
+            self.lbl_result.config(text="Default values are set.\n Please press \"Find answer\" again.")
+            return
+        
+        # Check if input is a number
+        try:
+            product_A_material = int(self.product_A_material.get())
+            product_B_material = int(self.product_B_material.get())
+            material_max = int(self.material_max.get())
+            product_A_labor = int(self.product_A_labor.get())
+            product_B_labor = int(self.product_B_labor.get())
+            labor_max = int(self.labor_max.get())
+        except ValueError:
+            self.lbl_result.config(text="Please input numbers only.")
+            return
+
+        # Check if one input is empty
+        if self.product_A_material.get() == "" or self.product_B_material.get() == "" or self.material_max.get() == "" or self.product_A_labor.get() == "" or self.product_B_labor.get() == "" or self.labor_max.get() == "":
+            self.lbl_result.config(text="Please input all required variables.")
+            return
+        # Call the function from func.py
+        result = func.product_mix_optimization(
+            product_A_material=int(self.product_A_material.get()),
+            product_B_material=int(self.product_B_material.get()),
+            material_max=int(self.material_max.get()),
+            product_A_labor=int(self.product_A_labor.get()),
+            product_B_labor=int(self.product_B_labor.get()),
+            labor_max=int(self.labor_max.get())
+        )
+        self.lbl_result.config(text=result)
+
+if __name__ == "__main__":
+    app = MainApp()
+    app.mainloop()
